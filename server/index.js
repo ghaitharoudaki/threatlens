a398e5ae3ee4fec05433bdae87b7b8aba9173d6f
Ghaith <ghaitharoudaki11@gmail.com>
2026-04-05 12:46:12 +0300

    fix cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ import analyzeRoute from './routes/analyze.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://https://phishing-analyzer.vercel.app/', 'http://localhost:5173'],
+  methods: ['GET', 'POST'],
+}));
 app.use(express.json());
 
 app.use('/api/analyze', analyzeRoute);
